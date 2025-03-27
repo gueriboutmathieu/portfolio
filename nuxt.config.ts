16adb55e03ef99dd222fdc0e94f4ae7bb247bed2
GUERIBOUT MATHIEU <gueribout.mathieu@protonmail.com>
2025-03-27 22:17:14 +0100

    release: 1.0.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
     ssr: false,
     compatibilityDate: "2024-11-01",
     devtools: { enabled: false },
+    app: {
+        baseURL: "/",
+    },
     srcDir: "src/",
     modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
     vite: {
