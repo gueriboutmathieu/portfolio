8c2bcc1dbc3f475f8b0f043de9d5c81284227fda
GUERIBOUT MATHIEU <gueribout.mathieu@protonmail.com>
2025-04-16 02:12:33 +0200

    feat: enhance projects section

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,28 +1,64 @@
 <template>
-    <div class="h-full w-full flex items-center justify-center relative">
-        <span class="absolute top-7 text-3xl font-bold">{{ language === Language.FR ? "Projets" : "Projects" }}</span>
-        <div class="flex flex-col items-center justify-center gap-5">
-            <div
-                v-for="project in projects"
-                class="w-2/3 flex flex-col items-start rounded-md bg-darkLight shadow-xl p-5 gap-5"
-            >
-                <span class="text-3xl">{{ project.title }}</span>
-                <span>{{ project.description }}</span>
-                <a :href="project.github" target="_blank" class="flex self-center">
-                    <FontAwesomeIcon icon="fa-brands fa-github" size="2x" class="text-white hover:text-purple" />
-                </a>
+    <div class="h-full w-full flex items-center justify-start relative">
+        <span class="absolute right-0 left-0 text-center top-7 text-3xl font-bold">{{ language === Language.FR ? "Projets" : "Projects" }}</span>
+
+        <div
+            v-for="project in projects"
+            class="max-h-full min-w-full flex items-center px-24 py-32 transition-transform duration-500 ease-in-out"
+            :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+        >
+            <div class="h-fit w-full flex flex-col items-start justify-start gap-5 bg-darkLight rounded-md shadow-xl p-5">
+                <img :src="project.image" class="rounded-md" />
+                <div class="flex flex-row items-start justify-center gap-5">
+                    <span class="text-3xl font-bold">{{ project.title }}</span>
+                    <a :href="project.github" target="_blank" class="flex self-center">
+                        <FontAwesomeIcon icon="fa-brands fa-github" size="2x" class="text-white hover:text-purple" />
+                    </a>
+                </div>
+                <p class="text-lg" v-html="project.description.replace(/\n/g, '<br />')" />
             </div>
-        </div>
+        </div>      
+
+        <FontAwesomeIcon
+            icon="fa-solid fa-arrow-left"
+            size="2x"
+            class="absolute left-5 rounded-full p-2.5 aspect-square text-white cursor-pointer hover:bg-lightBlue hover:text-dark"
+            @click="showPreviousProject"
+        />
+
+        <FontAwesomeIcon
+            icon="fa-solid fa-arrow-right"
+            size="2x"
+            class="absolute right-5 rounded-full p-2.5 aspect-square text-white cursor-pointer hover:bg-lightBlue hover:text-dark"
+            @click="showNextProject"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
-import type { MainStore } from "@/stores/mainStore";
 import { Language } from "@/models/language";
+import type { MainStore } from "@/stores/mainStore";
 
 const mainStore = useNuxtApp().$mainStore as MainStore;
 const projects = computed(function () {
     return mainStore.state.projects;
 });
+const currentIndex = ref<number>(0);
 const language = computed(() => mainStore.state.language);
+
+function showNextProject() {
+    if (currentIndex.value === projects.value.length - 1) {
+        currentIndex.value = 0;
+    } else {
+        currentIndex.value++;
+    }
+}
+
+function showPreviousProject() {
+    if (currentIndex.value === 0) {
+        currentIndex.value = projects.value.length - 1;
+    } else {
+        currentIndex.value--;
+    }
+}
 </script>
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,11 +2,15 @@ import type { Project } from "@/models/project";
 import nuxtImage from "@/assets/icons/nuxt.svg";
 import tailwindImage from "@/assets/icons/tailwind.svg";
 
+function getImageUrl(image: string) {
+    return new URL(`../assets/images/${image}`, import.meta.url).href;
+}
+
 export const frProjects: Project[] = [
     {
         title: "Portfolio",
         description: "Mon portfolio, le site que vous êtes en train d'explorer ✌️",
-        image: "/assets/portfolio.png",
+        image: getImageUrl("portfolio.png"),
         github: "https://github.com/gueriboutmathieu/portfolio",
         technologies: [
             { name: "Nuxt.js", icon: nuxtImage },
@@ -14,36 +18,43 @@ export const frProjects: Project[] = [
         ],
     },
     {
-        title: "Meri Server",
-        description:
-            "Le serveur de Meri, votre fidèle compagnon qui vous aide à rester organisé et à vous souvenir de ce qui compte vraiment.",
-        image: "/assets/meri.png",
-        github: "https://github.com/gueriboutmathieu/meri_server",
+        title: "Meri",
+        description: `Meri est un assistant LLM en speech to speech basé sur le principe du RAG (Retrieval Augmented Generation).
+            Contrairement à un chatbot classique, il peut mémoriser des informations, les stocker dans une base de connaissances, puis les utiliser pour répondre de manière plus pertinente.
+            Il permet donc, à l'instar d'un post-it, de stocker des informations importantes et de les retrouver facilement.`,
+        image: getImageUrl("meri.png"),
+        github: "https://github.com/gueriboutmathieu/meri_app",
         technologies: [
             { name: "Python", icon: "" },
             { name: "FastAPI", icon: "" },
             { name: "PostgreSQL", icon: "" },
+            { name: "OpenAI", icon: "" },
+            { name: "Nuxt.js", icon: "" },
+            { name: "TailwindCSS", icon: tailwindImage },
         ],
     },
     {
-        title: "Meri Web",
-        description:
-            "La web app de Meri, votre fidèle compagnon qui vous aide à rester organisé et à vous souvenir de ce qui compte vraiment.",
-        image: "/assets/meri.png",
-        github: "https://github.com/gueriboutmathieu/meri_web",
+        title: "Flare",
+        description: `Voyant Freetube ne plus fonctionner depuis quelques mois, j'avais besoin de trouver une nouvelle alternative pour regarder mes vidéos Youtube sans pub et sans algorithme.
+            J'ai donc décidé de me lancer pour coder ma propre solution : Flare !
+            C'est encore en construction et pas du tout utilisable pour l'instant mais j'y travaille !`,
+        image: getImageUrl("flare.png"),
+        github: "https://github.com/gueriboutmathieu/flare_app",
         technologies: [
             { name: "Python", icon: "" },
             { name: "FastAPI", icon: "" },
             { name: "PostgreSQL", icon: "" },
+            { name: "Nuxt.js", icon: "" },
+            { name: "TailwindCSS", icon: tailwindImage },
         ],
-    },
+    }
 ];
 
 export const enProjects: Project[] = [
     {
         title: "Portfolio",
         description: "My portfolio, the website you are currently exploring ✌️",
-        image: "/assets/portfolio.png",
+        image: getImageUrl("portfolio.png"),
         github: "https://github.com/gueriboutmathieu/portfolio",
         technologies: [
             { name: "Nuxt.js", icon: nuxtImage },
@@ -51,27 +62,34 @@ export const enProjects: Project[] = [
         ],
     },
     {
-        title: "Meri Server",
-        description:
-            "The server of Meri, your loyal companion that helps you stay organized and remember what truly matters.",
-        image: "/assets/meri.png",
-        github: "https://github.com/gueriboutmathieu/meri_server",
+        title: "Meri",
+        description: `Meri is a speech-to-speech LLM assistant based on the principle of RAG (Retrieval Augmented Generation).
+            Unlike a traditional chatbot, it can remember information, store it in a knowledge base, and use it later to provide more relevant answers.
+            Just like a smart post-it, it helps keep track of important information and makes it easy to retrieve when needed.`,
+        image: getImageUrl("meri.png"),
+        github: "https://github.com/gueriboutmathieu/meri_app",
         technologies: [
             { name: "Python", icon: "" },
             { name: "FastAPI", icon: "" },
             { name: "PostgreSQL", icon: "" },
+            { name: "OpenAI", icon: "" },
+            { name: "Nuxt.js", icon: "" },
+            { name: "TailwindCSS", icon: tailwindImage },
         ],
     },
     {
-        title: "Meri Web",
-        description:
-            "The web app of Meri, your loyal companion that helps you stay organized and remember what truly matters.",
-        image: "/assets/meri.png",
-        github: "https://github.com/gueriboutmathieu/meri_web",
+        title: "Flare",
+        description: `Since FreeTube hasn't been working for the past few months, I needed to find a new alternative to watch YouTube videos without ads and the algorithm.
+            So I decided to start coding my own solution: Flare !
+            It's still under construction and not usable yet, but I'm working on it !`,
+        image: getImageUrl("flare.png"),
+        github: "https://github.com/gueriboutmathieu/flare_app",
         technologies: [
             { name: "Python", icon: "" },
             { name: "FastAPI", icon: "" },
             { name: "PostgreSQL", icon: "" },
+            { name: "Nuxt.js", icon: "" },
+            { name: "TailwindCSS", icon: tailwindImage },
         ],
     },
 ];
