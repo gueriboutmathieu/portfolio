4eaf036c4e58ae7dbe5414eaf3434cd686929abe
gueriboutmathieu <gueriboutmathieu@users.noreply.github.com>
2025-04-24 23:13:25 +0000

    deploy: 6e12a97b5328a9381f055e2f29dd427d177ab900

---
+++ _nuxt/B71ymGXK.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DlAUqK2U.js";import{c as r,o as t,K as s}from"./7TZbuYE_.js";const c={};function n(e,a){return t(),r("em",null,[s(e.$slots,"default")])}const m=o(c,[["render",n]]);export{m as default};
