acd5ea2986d5d438b8ced637c10d49e5253b8917
GUERIBOUT MATHIEU <gueribout.mathieu@protonmail.com>
2025-04-25 01:12:06 +0200

    fix: prettier

--- src/services/chatbot.ts
+++ src/services/chatbot.ts
@@ -5,7 +5,12 @@ export type ChatbotService = {
     query: (prompt: string) => Promise<void>;
 };
 
-export function createChatbotService(baseUrl: string, publicKey: string, model: string, mainStore: MainStore): ChatbotService {
+export function createChatbotService(
+    baseUrl: string,
+    publicKey: string,
+    model: string,
+    mainStore: MainStore,
+): ChatbotService {
     const _baseUrl = baseUrl;
     const _headers: Record<string, any> = { "Content-Type": "application/json" };
     const _model = model;
@@ -22,11 +27,10 @@ export function createChatbotService(baseUrl: string, publicKey: string, model: 
             const body = await response.json();
             const accessToken = body.access_token;
             _headers["Authorization"] = `Bearer ${accessToken}`;
-        }
-        else {
+        } else {
             throw new Error(`Failed to initialize chatbot service, error : ${await response.json()}`);
         }
-    }
+    };
 
     const query = async function (prompt: string): Promise<void> {
         mainStore.addChatMessage({ role: "user", content: prompt });
@@ -38,10 +42,7 @@ export function createChatbotService(baseUrl: string, publicKey: string, model: 
         const chatMessages = mainStore.state.chatMessages;
         const body = JSON.stringify([{ role: "system", content: systemPrompt.value }, ...chatMessages]);
 
-        const response = await fetch(
-            url,
-            { method: "POST", headers: _headers, body: body }
-        );
+        const response = await fetch(url, { method: "POST", headers: _headers, body: body });
         const responseBody = await response.json();
 
         mainStore.addChatMessage(responseBody);
